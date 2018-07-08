3e7bc1137afaa13be8c78d088c302482b04aebb4
WuYuanhun <wu_yuanhun@Wu-YuanhundeMacBook-Pro.local>
2018-07-08 13:16:17 +0800

    daily update

--- team5308api.py
+++ team5308api.py
@@ -92,4 +92,24 @@ class team5308(object):
 
     def scanAndFix2(self):
         tempST = self.ST
-        tempST = cv2.
+        edges = cv2.Canny(tempST, 50, 150, apertureSize = 3)
+        lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
+        self.drawLines(self.png, lines)
+
+    def drawLines(self,img, lines):
+        for i in lines:
+            for (rho, theta) in i:
+                a = np.cos(theta)
+                b = np.sin(theta)
+#           变换到x-y坐标系
+                x0 = a * rho
+                y0 = b * rho
+#           用点和斜率得到直线上的两个点
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+
+        cv2.namedWindow('lines')
+        cv2.imshow('lines', img)
--- vision.py
+++ vision.py
@@ -9,7 +9,11 @@ cv2.imshow('testHSV',team.HSVPic)
 team.findcase()
 #team.scanAndFix()
 
+
+print("begin scan and Fix 2")
 team.scanAndFix2()
+print("scan and Fix completed")
+
 
 team.saveLocal(0)
 
